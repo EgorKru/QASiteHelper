c073edc54fe99ef0c72dcbf39745030be9081c48
Егор <krutakovegor43@gmail.com>
2024-10-13 23:09:39 +0300

    Правки по шапке, пока будет майном, сейчас динамику добавим и выравнивание + максимально описание каждого шага

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 import AboutPage from './components/AboutPage';
 import ConsultationPage from './components/ConsultationPage';
 import ContactPage from './components/ContactPage';
@@ -25,7 +24,7 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <div className="container">
-                        <h1>QA Helper Platform</h1>
+                        <Link to="/" className="header-title">QA Helper Platform</Link> {/* Заголовок теперь кликабельный */}
 
                         <div className="nav-bar">
                             <input
@@ -36,33 +35,22 @@ function App() {
                                 onChange={handleSearchChange}
                             />
                             <nav>
-                                <nav>
-                                    <Link to="/about">О проекте</Link>
-                                    <Link to="/consultation">Консультации</Link>
-                                    <Link to="/events">События</Link>
-                                    <Link to="/faq">FAQ</Link>
-                                    <Link to="/forum">Форум</Link>
-                                    <Link to="/contacts">Контакты</Link>
-                                    <Link to="/login" className="auth-button">Вход</Link>
-                                    <Link to="/register" className="auth-button">Регистрация</Link>
-                                </nav>
-
+                                <Link to="/login" className="auth-button">Вход</Link>
                             </nav>
                         </div>
                     </div>
                 </header>
 
                 <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="/about" element={<AboutPage />} />
-                        <Route path="/consultation" element={<ConsultationPage />} />
-                        <Route path="/contacts" element={<ContactPage />} />
-                        <Route path="/events" element={<EventsPage />} />
-                        <Route path="/faq" element={<FAQPage />} />
-                        <Route path="/forum" element={<ForumPage />} />
-                        <Route path="/reviews" element={<ReviewsPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/consultation" element={<ConsultationPage />} />
+                    <Route path="/contacts" element={<ContactPage />} />
+                    <Route path="/events" element={<EventsPage />} />
+                    <Route path="/faq" element={<FAQPage />} />
+                    <Route path="/forum" element={<ForumPage />} />
+                    <Route path="/reviews" element={<ReviewsPage />} />
                 </Routes>
             </div>
         </Router>
